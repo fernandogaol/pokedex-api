c55817d92360e1043219b71eb4f260f84a387890
Fernando Gaol <ffilho@riolindo.org>
2020-02-01 14:20:46 -0700

    edited lines 18-25

--- server.js
+++ server.js
@@ -11,21 +11,17 @@ app.use(helmet()); // gets rid of powered by = express on network dev tool
 app.use(cors());
 app.use(morgan(morganSetting));
 
-app.use((error, req, res, next) => {
-  if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    response = { error };
-  }
-  res.status(500).json(response);
-});
-
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+  if (!authToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  if (authToken.split(' ')[1] !== apiToken) {
+    return res
+      .status(401)
+      .json({ error: 'Invalid key. Please enter the correct key' });
   }
 
   next();
@@ -77,6 +73,15 @@ app.get('/pokemon', function handleGetPokemon(req, res) {
   }
 
   res.json(response);
+});
+
+app.use((error, req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
 
 const PORT = process.env.PORT || 8000;
